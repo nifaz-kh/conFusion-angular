73d2c2d57d94715805b814cd41035a291e0ba3f7
Hitesh Lad <hiteshklad@gmail.com>
2017-08-09 20:30:03 -0700

    Assignment Week 2 - SPA and routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,6 +16,7 @@ import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
 
 import { DishService } from './services/dish.service';
+import { LeaderService } from './services/leader.service';
 import { PromotionService } from './services/promotion.service';
 
 import { AppRoutingModule } from './app-routing/app-routing.module';
@@ -40,6 +41,7 @@ import { AppRoutingModule } from './app-routing/app-routing.module';
   ],
   providers: [
     DishService,
+    LeaderService,
     PromotionService
   ],
   bootstrap: [AppComponent]
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Dish } from '../shared/dish';
 import { DishService } from '../services/dish.service';
+import { Leader } from '../shared/leader';
+import { LeaderService } from '../services/leader.service';
 import { Promotion } from '../shared/promotion';
 import { PromotionService } from '../services/promotion.service';
 @Component({
@@ -11,17 +13,18 @@ import { PromotionService } from '../services/promotion.service';
 export class HomeComponent implements OnInit {
 
   dish: Dish;
+  maybeFeaturedLeader: Leader;
   promotion: Promotion;
 
   constructor(
     private dishService: DishService,
+    private leaderService: LeaderService,
     private promotionService: PromotionService) {
-
     }
 
   ngOnInit() {
     this.dish = this.dishService.getFeaturedDish();
     this.promotion = this.promotionService.getFeaturedPromotion();
+    this.maybeFeaturedLeader = this.leaderService.getFeaturedLeader();
   }
-
 }
